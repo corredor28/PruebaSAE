49724a7c4ce2fe5003c16c00d6c87fa8c35b73d5
Humberto Corredor <corredor28@gmail.com>
2017-12-02 10:53:13 -0500

    Incluir proyecto de pruebas unitarias

---
+++ PruebaSAE/PruebaSAE.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebaSAE.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebaSAE.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("971bba06-4565-4cb2-8c8e-e50c81445265")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PruebaSAE/PruebaSAE.Test/TestPadre.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PruebaSAE.Test.ServicioEntidades;
+
+namespace PruebaSAE.Test
+{
+    [TestClass]
+    public class TestPadre
+    {
+        [TestMethod]
+        public void TestCrearPadre()
+        {
+            var servicio = new EntidadesClient();
+            var nombrePrueba = "Padre prueba - " + DateTime.Now.ToString();
+            var respuesta = servicio.CrearPadre(nombrePrueba);
+            
+            // Validar objeto de respuesta
+            Assert.IsNotNull(respuesta);
+
+            // Validar resultado
+            Assert.IsTrue(respuesta.Estado);
+
+            // Ahora consultar para validar que existe
+            respuesta = servicio.ObtenerPadres();
+            
+            Assert.IsNotNull(respuesta.Padres);
+            var existePadre = false;
+            foreach (var padreWCF in respuesta.Padres)
+            {
+                if (padreWCF.Nombre == nombrePrueba)
+                {
+                    existePadre = true;
+                }
+            }
+            Assert.IsTrue(existePadre);
+        }
+    }
+}
